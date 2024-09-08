bc9df7d8477be435020b5cffdc0d8e71f59f2d37
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-09-08 11:46:54 +0000

    feat: Add routes for version 1 of categories and unmatched endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :categories, only: %i[index show create update destroy]
+    end
+  end
+
   # Define your application routes per the DSL in
   # https://guides.rubyonrails.org/routing.html
 
@@ -10,6 +16,5 @@ Rails.application.routes.draw do
   # app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  match '*unmatched', to: 'application#invalid_route', via: :all
 end
