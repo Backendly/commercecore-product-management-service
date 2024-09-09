26bf3fe41ee59d6fbbb3c3b96916da656881f50c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-09-09 09:19:33 +0000

    feat: Add serializer for Category model

---
+++ app/serializers/category_serializer.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Category Serializer
+class CategorySerializer
+  include JSONAPI::Serializer
+
+  attributes :name, :description, :developer_id, :created_at, :updated_at
+
+  cache_options store: Rails.cache, namespace: 'jsonapi-serializer',
+    expires_in: 1.hour
+
+  attribute :links do |category|
+    { self: Rails.application.routes.url_helpers.api_v1_category_url(category) }
+  end
+end
