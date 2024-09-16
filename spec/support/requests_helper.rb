23f89ffa206d9188db2f25150da1b8a5f399cf77
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-09-16 15:04:21 +0000

    refactor: Move authentication helper to a separate module

--- spec/support/requests_helper.rb
+++ spec/support/requests_helper.rb
@@ -4,8 +4,4 @@ module RequestsHelper
   def response_body
     JSON.parse(response.body, symbolize_names: true)
   end
-
-  # An authentication mocking helper for controller request testing
-  def authenticate_headers(headers, valid_dev_token: false,
-                           valid_user_id: false, valid_app_id: false) end
 end
