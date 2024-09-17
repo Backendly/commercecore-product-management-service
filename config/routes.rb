f999036c39f113a8ea82e21d4a670633b6e8d185
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-09-17 12:28:42 +0000

    feat: Add routes for adding and deleting images to and from products

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :products, only: %i[index show create update destroy]
+      resources :products, only: %i[index show create update destroy] do
+        member do
+          post :images, to: 'products#upload_images', as: :upload_images
+          delete 'images/:image_id', to: 'products#delete_image',
+                                     as: :delete_image
+        end
+      end
       resources :categories, only: %i[index show create update destroy]
     end
   end
