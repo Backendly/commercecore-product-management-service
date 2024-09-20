5c68b0ff8bcdb1b94123ba2dcbcb56168f01c77f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-09-20 01:21:46 +0000

    feat: Add helper module to handle cart creation and retrieval for controllers

---
+++ app/controllers/concerns/cart_helper.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Helper module for the cart resource.
+module CartHelper
+  # Retrieves the current user's cart.
+  #
+  # This method finds or creates a cart for the current user, app,
+  # and developer.
+  # It uses the cache to store the cart for future requests.
+  #
+  # === Parameters:
+  # * +user_id+ (String): The UUID of the user associated with the cart.
+  # * +app_id+ (String): The UUID of the app associated with the cart.
+  # * +developer_id+ (String): The UUID of the developer associated with
+  # the cart.
+  #
+  # === Returns:
+  # * (Cart): The current user's cart.
+  #
+  def cart
+    cache_resource("cart_#{developer_id}_#{app_id}_#{user_id}") do
+      Cart.find_or_create(user_id:, app_id:, developer_id:)
+    end
+  end
+end
