033b31fa25d3fbfe1307e73152a6890d33dc2d11
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-09-25 15:57:17 +0000

    feat: Refactor UserServiceClient to use new API endpoints
    This commit updates the `UserServiceClient` class to use the new API endpoints for validating developer tokens, users, and apps. It also improves error handling and caching logic. These changes enhance the reliability and performance of the user service client.

--- app/services/user_service_client.rb
+++ app/services/user_service_client.rb
@@ -11,33 +11,54 @@ class UserServiceClient
   #
   # @param developer_token [String] the developer token to validate
   # @return [String, nil] the developer ID if valid, nil otherwise
-  def fetch_developer_id(developer_token)
+  def fetch_developer_id(developer_token:)
     dev_token_key = "developer:#{developer_token}"
     cached_data = Rails.cache.fetch(dev_token_key)
+
     return cached_data if cached_data
 
     response = self.class.get(
-      '/validate_developer',
-      headers: { 'X-Developer-Token' => developer_token }
+      '/developer/validate-token',
+      headers: { 'X-API-Token' => developer_token }
     )
 
-    return unless response.success?
-
-    Rails.cache.fetch(dev_token_key, expires_in: 12.hours) do
-      response.parsed_response['developer_id']
+    if response.success?
+      Rails.cache.fetch(dev_token_key, expires_in: 12.hours) do
+        response.parsed_response.dig('developer', 'id')
+      end
+    else
+      Rails.logger.error 'Failed to validate developer token: ' \
+        "#{developer_token}"
+      nil
     end
   end
-
-  # rubocop:enable Metrics/MethodLength
 
   # Fetches and caches the user data based on the user ID.
   #
   # @param user_id [String] the user ID to validate
+  # @param app_id [String] the app ID to validate
+  # @param developer_token [String] the developer token to validate
   # @return [Hash, nil] the user data if valid, nil otherwise
-  def fetch_user(user_id)
-    Rails.cache.fetch("user:#{user_id}", expires_in: 12.hours) do
-      response = self.class.get("/validate_user/#{user_id}")
-      return response.parsed_response if response.success?
+  def fetch_user(user_id:, app_id:, developer_token:)
+    cache_key = "user:#{user_id}_app:#{app_id}_dev:#{developer_token}"
+    cached_data = Rails.cache.fetch(cache_key)
+
+    return cached_data if cached_data
+
+    response = self.class.get(
+      "/user/validate-user/#{user_id}", headers: {
+        'X-API-Token' => developer_token,
+        'X-App-Id' => app_id
+      }
+    )
+
+    if response.success?
+      Rails.cache.fetch(cache_key, expires_in: 12.hours) do
+        response.parsed_response
+      end
+    else
+      Rails.logger.error 'Failed to validate user: ' \
+        "#{user_id} for app: #{app_id}"
 
       nil
     end
@@ -46,13 +67,30 @@ class UserServiceClient
   # Fetches and caches the app data based on the app ID.
   #
   # @param app_id [String] the app ID to validate
+  # @param developer_token [String] the developer token to validate
   # @return [Hash, nil] the app data if valid, nil otherwise
-  def fetch_app(app_id)
-    Rails.cache.fetch("app:#{app_id}", expires_in: 12.hours) do
-      response = self.class.get("/validate_app/#{app_id}")
-      return response.parsed_response if response.success?
+  def fetch_app(app_id:, developer_token:)
+    cache_key = "app:#{app_id}_#{developer_token}"
+    cached_data = Rails.cache.fetch(cache_key)
 
+    return cached_data if cached_data
+
+    response = self.class.get(
+      "/app/validate-app/#{app_id}", headers: {
+        'X-API-Token' => developer_token
+      }
+    )
+
+    if response.success?
+      Rails.cache.fetch(cache_key, expires_in: 12.hours) do
+        response.parsed_response
+      end
+    else
+      Rails.logger.error 'Failed to validate app: ' \
+        "#{app_id} for developer: #{developer_token}"
       nil
     end
   end
 end
+
+# rubocop:enable Metrics/MethodLength
