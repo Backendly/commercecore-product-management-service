eb03b5b6c87eb9be6c65e90f6b911de8f24a3513
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-09-25 16:04:34 +0000

    feat: Update PaymentStatusJob to handle 'refunded' status
    This commit updates the `PaymentStatusJob` class to handle the 'refunded' status for orders. It adds the logic to update the order status to 'refunded' and perform necessary actions such as notifying the user and updating the product stock. Additionally, it introduces a private method `update_order_and_notify` to encapsulate the code for updating the order status and notifying the user. This change improves the functionality and flexibility of the payment status handling in the application.

--- app/jobs/payment_status_job.rb
+++ app/jobs/payment_status_job.rb
@@ -5,7 +5,6 @@ class PaymentStatusJob < ApplicationJob
   queue_as :high_priority
 
   # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/AbcSize
 
   def perform(order_id, status)
     order = Order.find_by(id: order_id)
@@ -15,26 +14,43 @@ class PaymentStatusJob < ApplicationJob
       return
     end
 
-    case status
-      when 'succeeded'
-        UpdateOrderStatusJob.perform_later(order.id, 'successful')
-        NotifyUserServiceJob.perform_later(order.id, 'successful')
-        UpdateProductStockJob.perform_later(order.id)
-        ClearCartJob.perform_later(order.user_id)
-      when 'created'
-        UpdateOrderStatusJob.perform_later(order.id, 'processing')
-        NotifyUserServiceJob.perform_later(order.id, 'processing')
-      when 'failed'
-        UpdateOrderStatusJob.perform_later(order.id, 'failed')
-        NotifyUserServiceJob.perform_later(order.id, 'failed')
-      else
-        Rails.logger.error "Invalid status: #{status}"
-        return
+    status_actions = {
+      succeeded: ['successful', true],
+      created: ['processing', false],
+      failed: ['failed', false],
+      refunded: ['refunded', true]
+    }
+
+    sym_status = status.to_sym
+
+    if status_actions.key?(sym_status)
+      new_status, update_stock = status_actions[sym_status]
+      update_order_and_notify(order.id, new_status)
+
+      if update_stock
+        UpdateProductStockJob.perform_later(order.id, status: new_status)
+      end
+
+      ClearCartJob.perform_later(order.user_id) if status == 'succeeded'
+    else
+      Rails.logger.error "Invalid status: #{status}"
+      return
     end
 
     OrderStatusNotificationJob.notify(order)
   end
 
+  private
+
+    def update_order_and_notify(order_id, status)
+      UpdateOrderStatusJob.perform_later(order_id, status)
+      NotifyUserServiceJob.perform_later(order_id, status)
+    end
+
+  # TODO: Implement logic to verify refund payment statuses
+  # An order should have been previously marked as successful to be refunded
+  # Communication between the payment service and the order service is required
+  # to verify the refund eligibility
+
   # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/AbcSize
 end
