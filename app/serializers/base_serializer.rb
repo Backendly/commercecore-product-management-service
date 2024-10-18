e7ef77604ac45111f705b230def058a61939ccd9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-10-18 16:39:14 +0000

    feat: Implement base serializer for application controller responses

---
+++ app/serializers/base_serializer.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Base serializer for all serializers in the application.
+class BaseSerializer
+  include JSONAPI::Serializer
+
+  cache_options store: Rails.cache, namespace: 'json-serializer',
+    expires_in: 1.day
+end
