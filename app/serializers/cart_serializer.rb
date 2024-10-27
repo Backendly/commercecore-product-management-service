814702f0519e03a9da435b057b962e37f513ec41
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-10-27 00:10:17 +0000

    feat: Add the 'updated_at' field to the response of the user's cart

--- app/serializers/cart_serializer.rb
+++ app/serializers/cart_serializer.rb
@@ -2,7 +2,7 @@
 
 # Serializer for the cart resource.
 class CartSerializer < BaseSerializer
-  attributes :user_id, :developer_id, :app_id, :created_at
+  attributes :user_id, :developer_id, :app_id, :created_at, :updated_at
 
   has_many :cart_items
 end
